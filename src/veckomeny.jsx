e51d385531a73315f97a1719f223c91769c2a645
Ludvig <ludvigvonreis@gmail.com>
2024-11-20 12:57:47 +0100

    added shadow and stuff

--- src/veckomeny.jsx
+++ src/veckomeny.jsx
@@ -20,7 +20,7 @@ const Veckomeny = () => {
 				const formattedMeals = {};
 
 				apiData.forEach((item, idx) => {
-					item.description = item.description.split("<br/>")
+					item.description = item.description.split("<br/>");
 					formattedMeals[idx] = item.description;
 				});
 
@@ -32,6 +32,9 @@ const Veckomeny = () => {
 
 		fetchMeals();
 	}, []);
+
+	const currentDay = new Date().getDay() - 1;
+	const glow = "drop-shadow-[0_0_16px_rgba(255,255,255,0.8)]";
 
 	return (
 		<div className="w-2/5 ml-4 p-5 rounded-2xl text-right shadow-lg bg-black/55 backdrop-blur-md">
@@ -45,8 +48,22 @@ const Veckomeny = () => {
 							className="flex justify-between text-white text-sm p-2 rounded gap-4 items-center"
 							key={day}
 						>
-							<span className="font-bold text-lg">{day}</span>
-							<span className="text-gray-200 text-base">
+							<span
+								className={`font-bold text-lg ${
+									idx === currentDay
+										? `text-purple-400 ${glow}`
+										: ""
+								}`}
+							>
+								{day}
+							</span>
+							<span
+								className={`text-gray-200 text-base ${
+									idx === currentDay
+										? `text-purple-400 ${glow}`
+										: ""
+								}`}
+							>
 								{meals[idx][0] || "Ingen data"}
 								<br />
 								{meals[idx][1] || "Ingen data"}
