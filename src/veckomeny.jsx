b81dc33c35a6a0b1e2cc47afaf2d1fbdbce8f828
Ludvig <ludvigvonreis@gmail.com>
2024-11-20 13:00:40 +0100

    added shadow and stuff

--- src/veckomeny.jsx
+++ src/veckomeny.jsx
@@ -19,10 +19,10 @@ const Veckomeny = () => {
         const apiData = response.data.items;
         const formattedMeals = {};
 
-        apiData.forEach((item, idx) => {
-          item.description = item.description.split("<br/>");
-          formattedMeals[idx] = item.description;
-        });
+				apiData.forEach((item, idx) => {
+					item.description = item.description.split("<br/>");
+					formattedMeals[idx] = item.description;
+				});
 
         setMeals(formattedMeals);
       } catch (error) {
@@ -30,31 +30,50 @@ const Veckomeny = () => {
       }
     };
 
-    fetchMeals();
-  }, []);
+		fetchMeals();
+	}, []);
 
-  return (
-    <div className="w-2/5 ml-4 p-5 rounded-2xl text-right shadow-lg bg-fuchsia-950/55 backdrop-blur-md">
-      <div className="text-4xl font-bold text-white p-2 rounded mb-5 text-center">
-        Veckans Lunch
-      </div>
-      <div className="space-y-2">
-        {["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag"].map((day, idx) => (
-          <div
-            className="flex justify-between text-white text-sm p-2 rounded gap-4 items-center"
-            key={day}
-          >
-            <span className="font-bold text-lg">{day}</span>
-            <span className="text-gray-200 text-base">
-              {meals[idx][0] || "Ingen data"}
-              <br />
-              {meals[idx][1] || "Ingen data"}
-            </span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	const currentDay = new Date().getDay() - 1;
+	const glow = "drop-shadow-[0_0_16px_rgba(255,255,255,0.8)]";
+
+	return (
+		<div className="w-2/5 ml-4 p-5 rounded-2xl text-right shadow-lg bg-black/55 backdrop-blur-md">
+			<div className="text-lg font-bold text-white p-2 rounded mb-5">
+				Veckans Lunch
+			</div>
+			<div className="space-y-2">
+				{["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag"].map(
+					(day, idx) => (
+						<div
+							className="flex justify-between text-white text-sm p-2 rounded gap-4 items-center"
+							key={day}
+						>
+							<span
+								className={`font-bold text-lg ${
+									idx === currentDay
+										? `text-purple-400 ${glow}`
+										: ""
+								}`}
+							>
+								{day}
+							</span>
+							<span
+								className={`text-gray-200 text-base ${
+									idx === currentDay
+										? `text-purple-400 ${glow}`
+										: ""
+								}`}
+							>
+								{meals[idx][0] || "Ingen data"}
+								<br />
+								{meals[idx][1] || "Ingen data"}
+							</span>
+						</div>
+					)
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Veckomeny;
