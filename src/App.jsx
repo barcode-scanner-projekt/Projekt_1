53352f88520d5c3e2aefc1b1b2449142bf6ccc0b
Ludvig <ludvigvonreis@gmail.com>
2024-11-25 12:01:23 +0100

    Added nti logo

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export const ScanCount = atom(0);
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App over">
       <div className="bg-transparent min-h-screen grid grid-cols-3 grid-rows-1 gap-4 p-8">
         <div className="w-auto h-screen flex flex-col gap-5 ml-5">
           <DataFetcher />
@@ -20,6 +20,7 @@ const App = () => {
 
         <Veckomeny />
       </div>
+	  <img className="absolute right-10 bottom-10 w-64 h-64" src="nti-logo.png" alt=""/>
       <Bubbles />
     </div>
   );
